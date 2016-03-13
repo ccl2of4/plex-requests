ecd2fe8924bba648cd59c81f1bfd1fff90476d5d
Connor Lirot <ccl2of4@tx.rr.com>
2016-03-13 17:31:56 -0500

    use foreign keys for cascade delete, use dict_factory for queries

--- server/database.py
+++ server/database.py
@@ -2,11 +2,20 @@ import sqlite3
 
 class Database:
 
+    @staticmethod
+    def dict_factory(cursor, row):
+        d = {}
+        for idx,col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
     def __init__(self):
         self.create_db()
 
     def get_conn(self):
         conn = sqlite3.connect('requests.db')
+        conn.execute('PRAGMA foreign_keys = ON;')
+        conn.row_factory = Database.dict_factory
         conn.isolation_level = None
         return conn
 
@@ -22,24 +31,21 @@ class Database:
             request_id integer,
             content text,
             date integer,
-            foreign key (request_id) references request(request_id) on delete cascade);''')
+            foreign key (request_id) references requests(request_id) on delete cascade);''')
 
     def get_requests(self):
         conn = self.get_conn()
         query = conn.execute('SELECT * FROM requests')
-        colname = [ d[0] for d in query.description ]
-        result_list = [ dict(zip(colname, r)) for r in query.fetchall() ]
+        result_list = query.fetchall()
         for request in result_list:
             query = conn.execute('SELECT * FROM comments where request_id=? ORDER BY date ASC', (str(request['request_id']),))
-            colname = [ d[0] for d in query.description ]
-            comment_list = [ dict(zip(colname, r)) for r in query.fetchall() ]
+            comment_list = query.fetchall()
             request['comments'] = comment_list
         return result_list
 
     def delete_request(self, request_id):
         conn = self.get_conn()
         conn.execute('DELETE FROM requests WHERE request_id=?', (request_id,) )
-        conn.execute('DELETE FROM comments WHERE request_id=?', (request_id,) )
 
     def add_request(self, item):
         conn = self.get_conn()
@@ -50,6 +56,5 @@ class Database:
         conn.execute('INSERT INTO comments (request_id, content, date) VALUES (?,?,CURRENT_TIMESTAMP)', (request_id, comment['content']))
 
     def delete_comment(self, request_id, comment_id):
-        print(request_id, comment_id)
         conn = self.get_conn()
         conn.execute('DELETE FROM comments WHERE request_id=? AND comment_id=?', (request_id, comment_id))
