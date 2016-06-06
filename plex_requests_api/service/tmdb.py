d5e23a3b8901a3fca38ee62e9316dc9444c567e3
Connor Lirot <ccl2of4@tx.rr.com>
2016-06-05 21:14:09 -0500

    move url resolve logic into tmdb service clasS

--- plex_requests_api/service/tmdb.py
+++ plex_requests_api/service/tmdb.py
@@ -18,8 +18,8 @@ class TMDBService(object):
             movie = {}
             movie['name'] = r.get('title')
             movie['date'] = r.get('release_date')
-            movie['poster_path'] = resolve_img_url(r.get('poster_path'))
-            movie['backdrop_path'] = resolve_img_url(r.get('backdrop_path'))
+            movie['poster_path'] = self.resolve_img_url(r.get('poster_path'))
+            movie['backdrop_path'] = self.resolve_img_url(r.get('backdrop_path'))
             movie['summary'] = r.get('overview')
             movie['type'] = 'movie'
             return movie
@@ -39,13 +39,18 @@ class TMDBService(object):
             tvshow = {}
             tvshow['name'] = s.get('name')
             tvshow['date'] = s.get('first_air_date')
-            tvshow['poster_path'] = resolve_img_url(s.get('poster_path'))
-            tvshow['backdrop_path'] = resolve_img_url(s.get('backdrop_path'))
+            tvshow['poster_path'] = self.resolve_img_url(s.get('poster_path'))
+            tvshow['backdrop_path'] = self.resolve_img_url(s.get('backdrop_path'))
             tvshow['summary'] = s.get('overview')
             tvshow['type'] = 'tv_show'
             return tvshow
 
         return [transform(r) for r in s.results]
+
+    def resolve_img_url(self, url):
+        if None == url:
+            return None
+        return 'https://image.tmdb.org/t/p/' + 'w92' + url
 
     def handle_error(self, e):
         from .. import app
